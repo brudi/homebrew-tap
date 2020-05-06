6377d85bb5c073364134e3c107fdd71d8c3ba830
brudicloud <brudicloud@brudi.com>
2020-05-06 11:48:56 +0200

    Brew formula update for cli version v0.2.6

--- cli.rb
+++ cli.rb
@@ -2,18 +2,20 @@
 class Cli < Formula
   desc "brudi CLI"
   homepage "https://brudi.com/"
-  version "0.2.6-rc.2"
+  version "0.2.6"
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.mycompany.com/foo/bar/releases/v0.2.6-rc.2/cli_0.2.6-rc.2_macOS.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "46de016b4ea6a78b180c2ac461e06d79897b6055c670ef68ac13234f68484c21"
+    url "http://github.com/brudi/brudi-cli/releases/v0.2.6/cli_0.2.6_macOS.tar.gz"
+    sha256 "49740d2d8fe79d1e7516590836114b497687e745ee0956b9befdc91c4577a230"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "http://github.mycompany.com/foo/bar/releases/v0.2.6-rc.2/cli_0.2.6-rc.2_linux.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "8568a2c6079f7dd46b42d4a8335660fbfa1afd1cf3bd57526e755c6186cc97cd"
+      url "http://github.com/brudi/brudi-cli/releases/v0.2.6/cli_0.2.6_linux.tar.gz"
+      sha256 "efe8eda426b949eb10862e471d7041f255ec50f8484f64079659c319be0ebb3f"
     end
   end
+  
+  next "https://github.com/brudi/brudi-cli.git"
   
   depends_on "docker"
   depends_on "git"
