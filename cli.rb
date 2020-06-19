132ab93813d96607ecd7ad7db2e3c4a29e866db9
brudicloud <brudicloud@brudi.com>
2020-06-19 17:19:47 +0200

    Brew formula update for cli version v0.2.13

--- cli.rb
+++ cli.rb
@@ -3,16 +3,16 @@ require_relative "./lib/custom_download_strategy"
 class Cli < Formula
   desc "brudi CLI"
   homepage "https://brudi.com/"
-  version "0.2.11"
+  version "0.2.13"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/brudi/cli/releases/download/v0.2.11/cli_0.2.11_macOS.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "be9385ccbb55ea76e014d315ee50c85f08c22037bb99004f97bd2e8ccabe5cd7"
+    url "https://github.com/brudi/cli/releases/download/v0.2.13/cli_0.2.13_macOS.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "44211e04eb49a98a8b856a4f3e218b0686775474d2f466912ddcc2747276897d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/brudi/cli/releases/download/v0.2.11/cli_0.2.11_linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "b7acde67b53a647c92dbe6c68ead88e9faeae3cedbc02db7ad443693ed74e3fc"
+      url "https://github.com/brudi/cli/releases/download/v0.2.13/cli_0.2.13_linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "257d6d0087b7ecac0b4be0542e4f0f6dc6c8327d0d8483d39be7a2fb48701143"
     end
   end
 
